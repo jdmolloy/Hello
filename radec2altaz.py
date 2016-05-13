b128e5db09b536800c87663503c638772172e49b
Jeffrey D. Molloy <jdmolloy2012@berkeley.edu>
2016-05-13 16:32:53 -0700

    Add files via upload
    Check matrix multiplication under transform

--- radec2altaz.py
+++ radec2altaz.py
@@ -1,19 +1,19 @@
 import numpy as np, pylab
 import scipy as np
 
-def radec2altaz(ra, dec, lst, lat, long):
-	n = np.arange(ra)
-	alt = transpose(dblarr(n))
-	az = transpose(dblarr(n))
-	lst = transpose(dblarr(n))
+def radec2altaz(ra, dec, lst, lat, lon):
+	n = len(ra)
+	alt = np.zeros((n, 1))
+	az = np.zeros((n, 1))
+	lst = np.zeros((n, 1))
 
 # Inputs should be in decimal form. If not, then apply ten().
 # Please stick with one pair of (latitude, longitude) at a time.
-	if np.arange(lat) == 0:
+	if len(lat) == 0:
 		lat = np.float64(18.3538056) 
 # Default value for Arecibo Observatory (37.873199 degrees North at UCB).
-	if np.arange(long) == 0: 
-		long = np.float64(-66.7552222) 
+	if len(lon) == 0: 
+		lon = np.float64(-66.7552222) 
 # Default at Arecibo (-122.257063 degrees West at UCB).
 # timezone = UTC - 4
 # hour angle ha + ra = LST
@@ -31,34 +31,35 @@ def radec2altaz(ra, dec, lst, lat, long):
 		x_0 = np.float64(np.cos(delta) * np.cos(alpha))
 		x_1 = np.float64(np.cos(delta) * np.sin(alpha))
 		x_2 = np.float64(np.sin(delta))
-		x = np.array([x_0, x_1, x_2])
+	#	x = np.array([x_0, x_1, x_2])  Matrix multiplication (3 by 3) * (1 by 3) doesn't work
+		x = np.matrix([[x_0], [x_1], [x_2]])
 
 # Rotation matrices.
-		transform1 = np.array([[np.cos(lst_rad), np.sin(lst_rad), 0.0], [np.sin(lst_rad), -np.cos(lst_rad), 0.0], [0.0, 0.0, 1.0]])
-		transform2 = np.array([[-np.sin(phi), 0.0, np.cos(phi)], [0.0, -1.0, 0.0], [np.cos(phi), 0.0, np.sin(phi)]])
-		transform = transform2 ## transform1 ## x
-		alt[i] = np.float64(np.asin(transform(2)) * 180.0 / np.pi)
-		az[i] = np.float(np.atan(transform(1), transform(0)) * 180.0 / np.pi)
+		transform1 = np.matrix([[np.cos(lst_rad), np.sin(lst_rad), 0.0], [np.sin(lst_rad), -np.cos(lst_rad), 0.0], [0.0, 0.0, 1.0]])
+		transform2 = np.matrix([[-np.sin(phi), 0.0, np.cos(phi)], [0.0, -1.0, 0.0], [np.cos(phi), 0.0, np.sin(phi)]])
+		transform = transform2 * transform1 * x
+		alt[i] = np.arcsin(transform[2]) * 180.0 / np.pi
+		az[i] = np.arctan2(transform[1], transform[0]) * 180.0 / np.pi
 
 		if az[i] < 0.0:
 			az[i] = az[i] + 360.0
 		if az[i] > 360.0:
 			az[i] = az[i] - 360.0
 
-	return [alt, az]
+	return np.array([alt, az])
 
 
-def hadec2altaz(ha, dec, lat, long):
+def hadec2altaz(ha, dec, lat, lon):
 
-	n = np.arange(ha)
-	alt = transpose(dblarr(n))
-	az = transpose(dblarr(n))
+	n = len(ha)
+	alt = np.zeros((n, 1))
+	az = np.zeros((n, 1))
 
-	if np.arange(lat) == 0: 
+	if len(lat) == 0: 
 		lat = np.float64(18.3538056)
 # Default value for Arecibo Observatory (37.873199 degrees North at UCB).  
-	if np.arange(long) == 0: 
-		long = np.float64(-66.7552222)
+	if len(lon) == 0: 
+		lon = np.float64(-66.7552222)
 # Default at Arecibo (-122.257063 degrees West at UCB).
 
 	for num in ha:
@@ -75,14 +76,14 @@ def hadec2altaz(ha, dec, lat, long):
 		x_2 = np.float64(np.sin(delta))
 		x = np.array([x_0, x_1, x_2])
 
-		transform2 = np.array([[-np.sin(phi), 0.0, np.cos(phi)], [0.0, -1.0, 0.0], [np.cos(phi), 0.0, np.sin(phi)]])
-		transform = transform2 ## x
-		alt[i] = np.float64(np.asin(transform(2)) * 180.0 / np.pi)
-		az[i] = np.float64(np.atan(transform(1), transform(0)) * 180.0 / np.pi)
+		transform2 = np.matrix([[-np.sin(phi), 0.0, np.cos(phi)], [0.0, -1.0, 0.0], [np.cos(phi), 0.0, np.sin(phi)]])
+		transform = x * transform2  # Alternative solution to matrix multiplication problem above
+		alt[i] = np.arcsin(transform[2]) * 180.0 / np.pi
+		az[i] = np.arctan2(transform[1], transform[0]) * 180.0 / np.pi
 
 		if az[i] < 0.0: 
 			az[i] = az[i] + 360.0
 		if az[i] > 360.0: 
 			az[i] = az[i] + 360.0
 
-	return [alt, az]
+	return np.array([alt, az])
